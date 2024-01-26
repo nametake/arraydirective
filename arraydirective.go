5272169a8248055747b3fbb3e0ce92ba8323ee23
nametake <nametake.kyarabuki@gmail.com>
2024-01-26 19:39:08 +0900

    Fixed flags

--- arraydirective.go
+++ arraydirective.go
@@ -2,7 +2,6 @@ package arraydirective
 
 import (
 	"slices"
-	"strings"
 
 	"github.com/vektah/gqlparser/v2/ast"
 
@@ -11,61 +10,53 @@ import (
 
 const doc = "arraydirective is ..."
 
-// Analyzer is ...
-var Analyzer = &gqlanalysis.Analyzer{
-	Name: "arraydirective",
-	Doc:  doc,
-	Run:  run,
+// NewAnalyzer is ...
+func NewAnalyzer(types, directives []string) *gqlanalysis.Analyzer {
+	return &gqlanalysis.Analyzer{
+		Name: "arraydirective",
+		Doc:  doc,
+		Run:  run(types, directives),
+	}
 }
 
-var (
-	flagTypes      string
-	flagDirectives string
-)
+func run(types, directives []string) func(pass *gqlanalysis.Pass) (interface{}, error) {
+	return func(pass *gqlanalysis.Pass) (interface{}, error) {
 
-func init() {
-	Analyzer.Flags.StringVar(&flagTypes, "types", "", "target types")
-	Analyzer.Flags.StringVar(&flagDirectives, "directives", "", "must directives")
-}
-
-func run(pass *gqlanalysis.Pass) (interface{}, error) {
-	types := strings.Split(flagTypes, ",")
-	directives := strings.Split(flagDirectives, ",")
-
-	for _, t := range pass.Schema.Types {
-		if t.BuiltIn {
-			continue
-		}
-		if t.Kind == ast.InputObject {
-			for _, field := range t.Fields {
-				if field != nil && field.Type != nil && field.Type.Elem != nil {
-					if !slices.Contains(types, field.Type.Elem.NamedType) {
-						continue
-					}
-					for _, directive := range directives {
-						if field.Directives.ForName(directive) == nil {
-							pass.Reportf(field.Position, "%s has %s directive", field.Name, directive)
+		for _, t := range pass.Schema.Types {
+			if t.BuiltIn {
+				continue
+			}
+			if t.Kind == ast.InputObject {
+				for _, field := range t.Fields {
+					if field != nil && field.Type != nil && field.Type.Elem != nil {
+						if !slices.Contains(types, field.Type.Elem.NamedType) {
+							continue
+						}
+						for _, directive := range directives {
+							if field.Directives.ForName(directive) == nil {
+								pass.Reportf(field.Position, "%s has %s directive", field.Name, directive)
+							}
 						}
 					}
 				}
 			}
-		}
-		if t.Kind == ast.Object {
-			for _, field := range t.Fields {
-				for _, arg := range field.Arguments {
-					if arg != nil && arg.Type != nil && arg.Type.Elem != nil {
-						if !slices.Contains(types, arg.Type.Elem.NamedType) {
-							continue
-						}
-						for _, directive := range directives {
-							if arg.Directives.ForName(directive) == nil {
-								pass.Reportf(arg.Position, "argument %s of %s has no %s directive", arg.Name, arg.Name, directive)
+			if t.Kind == ast.Object {
+				for _, field := range t.Fields {
+					for _, arg := range field.Arguments {
+						if arg != nil && arg.Type != nil && arg.Type.Elem != nil {
+							if !slices.Contains(types, arg.Type.Elem.NamedType) {
+								continue
+							}
+							for _, directive := range directives {
+								if arg.Directives.ForName(directive) == nil {
+									pass.Reportf(arg.Position, "argument %s of %s has no %s directive", arg.Name, arg.Name, directive)
+								}
 							}
 						}
 					}
 				}
 			}
 		}
+		return nil, nil
 	}
-	return nil, nil
 }
--- cmd/arraydirective/main.go
+++ cmd/arraydirective/main.go
@@ -1,8 +1,23 @@
 package main
 
 import (
+	"flag"
+	"strings"
+
 	"github.com/gqlgo/gqlanalysis/multichecker"
 	"github.com/nametake/arraydirective"
 )
 
-func main() { multichecker.Main(arraydirective.Analyzer) }
+func main() {
+	var (
+		flagTypes      string
+		flagDirectives string
+	)
+
+	flag.StringVar(&flagTypes, "types", "ID", "target types")
+	flag.StringVar(&flagDirectives, "directives", "list", "must directives")
+
+	types := strings.Split(flagTypes, ",")
+	directives := strings.Split(flagDirectives, ",")
+	multichecker.Main(arraydirective.NewAnalyzer(types, directives))
+}
